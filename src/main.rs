8b72d42e369c462ab7e8d9788b5027c51d7cc960
Benno Richters <b.richters@gmail.com>
2025-12-10 09:10:28 +0100

    renamed 'dot' to 'pixel'

--- src/main.rs
+++ src/main.rs
@@ -24,8 +24,8 @@ fn main() {
     let size = 500;
     let delay = 4;
 
-    let dots = walk(steps, gradient);
-    show_extremes(&dots);
+    let pixels = walk(steps, gradient);
+    show_extremes(&pixels);
 
     let file = File::create("output.gif").unwrap();
     let mut encoder = Encoder::new(file, size as u16, size as u16, &[]).unwrap();
@@ -36,8 +36,8 @@ fn main() {
     for (c, projection) in projection_it.enumerate() {
         println!("{}", c);
 
-        let dot2ds = map_to_dot2d(&dots, projection);
-        let imgbuf = image(dot2ds);
+        let pixels2d = map_to_pixel2d(&pixels, projection);
+        let imgbuf = image(pixels2d);
 
         let rgba = DynamicImage::ImageRgb8(imgbuf).to_rgba8();
         let mut frame = Frame::from_rgba_speed(size as u16, size as u16, &mut rgba.into_raw(), 10);
@@ -48,10 +48,10 @@ fn main() {
     drop(encoder);
 }
 
-fn show_extremes(dots: &[Dot3D]) {
-    let (min_x, max_x) = extremes(dots, |e| e.coordinate.x);
-    let (min_y, max_y) = extremes(dots, |e| e.coordinate.y);
-    let (min_z, max_z) = extremes(dots, |e| e.coordinate.z);
+fn show_extremes(pixels: &[Pixel3D]) {
+    let (min_x, max_x) = extremes(pixels, |e| e.coordinate.x);
+    let (min_y, max_y) = extremes(pixels, |e| e.coordinate.y);
+    let (min_z, max_z) = extremes(pixels, |e| e.coordinate.z);
 
     println!(
         "({}, {}, {}), ({}, {}, {})",
@@ -59,39 +59,39 @@ fn show_extremes(dots: &[Dot3D]) {
     );
 }
 
-fn extremes<F>(dots: &[Dot3D], f: F) -> (f64, f64)
+fn extremes<F>(pixels: &[Pixel3D], f: F) -> (f64, f64)
 where
-    F: Fn(&&Dot3D) -> f64,
+    F: Fn(&&Pixel3D) -> f64,
 {
-    let compare = |a: &&Dot3D, b: &&Dot3D| f(a).total_cmp(&f(b));
-    let min = dots.iter().min_by(compare).unwrap();
-    let max = dots.iter().max_by(compare).unwrap();
+    let compare = |a: &&Pixel3D, b: &&Pixel3D| f(a).total_cmp(&f(b));
+    let min = pixels.iter().min_by(compare).unwrap();
+    let max = pixels.iter().max_by(compare).unwrap();
 
     (f(&min), f(&max))
 }
 
-fn map_to_dot2d(dots: &[Dot3D], projection: Projection) -> Vec<Dot2D> {
-    let mut dot2ds: Vec<Dot2D> = vec![];
-    for dot in dots {
-        let dist_coord_option = projection.project(&dot.coordinate);
+fn map_to_pixel2d(pixels: &[Pixel3D], projection: Projection) -> Vec<Pixel2D> {
+    let mut pixels2d: Vec<Pixel2D> = vec![];
+    for pixel in pixels {
+        let dist_coord_option = projection.project(&pixel.coordinate);
         if let Some((distance, coord)) = dist_coord_option {
             let x = coord.0.round() as i16;
             let y = coord.1.round() as i16;
 
-            let index_option = dot2ds.iter().position(|e| e.x == x && e.y == y);
+            let index_option = pixels2d.iter().position(|e| e.x == x && e.y == y);
             let mut to_push = true;
             if let Some(index) = index_option {
-                let existing = &dot2ds[index];
+                let existing = &pixels2d[index];
                 if distance < existing.distance {
-                    dot2ds.remove(index);
+                    pixels2d.remove(index);
                 } else {
                     to_push = false;
                 }
             }
 
-            let color = dot.color;
+            let color = pixel.color;
             if to_push {
-                dot2ds.push(Dot2D {
+                pixels2d.push(Pixel2D {
                     x,
                     y,
                     color,
@@ -100,15 +100,15 @@ fn map_to_dot2d(dots: &[Dot3D], projection: Projection) -> Vec<Dot2D> {
             }
         }
     }
-    dot2ds
+    pixels2d
 }
 
-fn image(dot2ds: Vec<Dot2D>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+fn image(pixels2d: Vec<Pixel2D>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     let size = 500;
     let half_size = (size / 2) as i16;
     let mut imgbuf = ImageBuffer::new(size, size);
 
-    for d in dot2ds {
+    for d in pixels2d {
         if d.x >= -half_size && d.x < half_size && d.y >= -half_size && d.y < half_size {
             let x = (half_size + d.x) as u32;
             let y = (half_size + d.y) as u32;
@@ -122,7 +122,7 @@ fn image(dot2ds: Vec<Dot2D>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
 }
 
 #[derive(Debug)]
-struct Dot2D {
+struct Pixel2D {
     x: i16,
     y: i16,
     color: (u8, u8, u8),
@@ -156,7 +156,7 @@ impl Iterator for DirIterator {
     }
 }
 
-fn walk(steps: usize, mut gradient: ColorGradient) -> Vec<Dot3D> {
+fn walk(steps: usize, mut gradient: ColorGradient) -> Vec<Pixel3D> {
     let mut result = vec![];
 
     let mut x = 0;
@@ -175,7 +175,7 @@ fn walk(steps: usize, mut gradient: ColorGradient) -> Vec<Dot3D> {
         }
 
         let color = gradient.next().unwrap();
-        result.push(Dot3D {
+        result.push(Pixel3D {
             coordinate: Tuple3D {
                 x: x as f64,
                 y: y as f64,
@@ -192,7 +192,7 @@ fn walk(steps: usize, mut gradient: ColorGradient) -> Vec<Dot3D> {
     result
 }
 
-struct Dot3D {
+struct Pixel3D {
     coordinate: Tuple3D,
     color: (u8, u8, u8),
 }
